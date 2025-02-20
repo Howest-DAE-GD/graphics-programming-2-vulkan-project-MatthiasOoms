722bdace1c3cd2ba386b41317fc8820ae4fc7072
MatthiasOoms <matthias.ooms1@gmail.com>
2025-02-20 16:50:30 +0100

    Implemented Vulkan instance creation

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -21,6 +22,7 @@ public:
 private:
 	// Member variables
     GLFWwindow* m_pWindow;
+    VkInstance m_Instance;
 
     void InitWindow() 
     {
@@ -34,7 +36,51 @@ private:
 
     void InitVulkan() 
     {
+        CreateInstance();
+    }
 
+    void CreateInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions{ glfwGetRequiredInstanceExtensions(&glfwExtensionCount) };
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &m_Instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &m_Instance) != VK_SUCCESS) 
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		
+#ifdef _DEBUG
+        std::cout << "available extensions:\n";
+        for (const auto& extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+#endif
     }
 
     void MainLoop() 
@@ -47,6 +93,8 @@ private:
 
     void Cleanup() 
     {
+        vkDestroyInstance(m_Instance, nullptr);
+
         glfwDestroyWindow(m_pWindow);
 
         glfwTerminate();
